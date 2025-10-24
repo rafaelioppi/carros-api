2e8eae6c5fd0423ffa7b30b29a89dbd19dbaa425
rafaelioppi <141973185+rafaelioppi@users.noreply.github.com>
2025-10-24 11:22:53 +0000

    rafael

---
+++ carros-api/src/test/java/com/rafael/carros_api/CarrosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.carros_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
